3e7c8e224f7487f39eb8cceca8a0b49d40cf80cf
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-04-22 11:41:50 +0530

    updates

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  const age = 20;
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name="Shiv" age={age} />
+    </div>
+  );
+};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
