5532d3e027296899af8a220cf5ff8d7c4a94e6fc
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-04-29 14:35:41 +0530

    Exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = ({ name, handleClick }) => {
+  return <button onClick={handleClick}>{name}</button>;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <br />
+      <div>Statistics</div>
+      <br />
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </>
+  );
+};
+
+const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
+  return (
+    <>
+      <div>Give feedback</div>
+      <br />
+      <Button name="good" handleClick={handleGood} />
+      <Button name="neutral" handleClick={handleNeutral} />
+      <Button name="bad" handleClick={handleBad} />
+      <br />
+    </>
+  );
+};
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const handleGood = () => {
+    console.log("Good");
+    setGood(good + 1);
+  };
+  const handleNeutral = () => {
+    console.log("Neutral");
+    setNeutral(neutral + 1);
+  };
+  const handleBad = () => {
+    console.log("Bad");
+    setBad(bad + 1);
+  };
+  return (
+    <>
+      <Feedback
+        handleGood={handleGood}
+        handleNeutral={handleNeutral}
+        handleBad={handleBad}
+      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
