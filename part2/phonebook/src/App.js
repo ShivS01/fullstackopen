0333a17ceb9d3930ce2c40bc312fc754a6a22ca0
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-04-30 17:47:26 +0530

    Till exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Phonebook from "./components/Phonebook";
+import Numbers from "./components/Numbers";
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-function App() {
+  const changeInput = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addNew = (event) => {
+    event.preventDefault();
+    if (persons.find((person) => person.name === newName))
+      return alert(`${newName} is already added to the phonebook`);
+    const obj = {
+      name: newName,
+    };
+    setPersons(persons.concat(obj));
+    setNewName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNew}>
+        <div>
+          name: <input value={newName} onChange={changeInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <div key={person.name}>{person.name}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ part2/phonebook/src/components/Phonebook.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Phonebook = () => {};
+
+export default Phonebook;
