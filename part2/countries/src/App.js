b9843a43597b91184bb0c218d1ff310f3a361abb
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-05-02 18:06:23 +0530

    2.12* Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [find, setFind] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
+  const changeFind = (event) => setFind(event.target.value);
+
+  const toShow = data.filter((entry) =>
+    entry.name.toLowerCase().includes(find)
+  );
+
+
+  const checkFilter = () => {
+    console.log(toShow);
+    if (toShow.length === 1) {
+      return (
+        toShow.map((e) => {
+          return (
+            <div key={e.name}>
+              <h1>{e.name}</h1>
+              <div>capital {e.capital}</div>
+              <div>population {e.population}</div>
+              <h2>languages</h2>
+              <ul>{e.languages.map(lang => <li key={lang.name}>{lang.name}</li>)}</ul>
+              <img src={e.flag}></img>
+            </div>
+          )
+        })
+      );
+    }
+    else if (toShow.length <= 10)
+      return toShow.map((each) => <div key={each.name}>{each.name}</div>);
+    else if (toShow.length > 10)
+      return "too many matches, specify another field";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>find countries</div>
+      <input onChange={changeFind} />
+      <div>{checkFilter()}</div>
     </div>
   );
 }
