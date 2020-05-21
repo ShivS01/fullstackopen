f423a623113e602dedbf1bc412e82eececb26426
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-05-21 13:58:48 +0530

    3.13: Phonebook database, step1:

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,42 +1,42 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const url = process.env.MONGODB_URI;
-// console.log("connecting to", url);
+const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
 
-// // const connectDB = async () => {
-// //   try {
-// //     const conn = await mongoose.connect(url, {
-// //       useNewUrlParser: true,
-// //       useUnifiedTopology: true,
-// //     });
-// //     console.log(`MongoDB connected: ${conn.connection.host}`);
-// //   } catch (err) {
-// //     console.log(`error: ${err.message}`);
-// //     process.exit(1);
-// //   }
-// // };
-// // connectDB();
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     console.log(`error: ${err.message}`);
+//     process.exit(1);
+//   }
+// };
+// connectDB();
 
-// mongoose
-//   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then((result) => {
-//     console.log("connected to MongoDB");
-//   })
-//   .catch((error) => {
-//     console.log("error connecting to MongoDB:", error.message);
-//   });
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
-// const personSchema = new mongoose.Schema({
-//   name: String,
-//   number: Number,
-// });
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
 
-// personSchema.set("toJSON", {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString();
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//   },
-// });
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-// module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema);
